ee0e707e2bda07c83f45478956d293acba56d4d8
Miguel Ballén <migaballengal@unal.edu.co>
2018-03-14 23:42:25 -0500

    Feat: My first epic function called from html

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
 // JS Here!
+function myFunction() {
+    console.log("Hello World");
+}
+
+function 
+
+function careerEvent(){
+    let body = this.document.getElementById("body");
+    body.setNewProperties();
+}
