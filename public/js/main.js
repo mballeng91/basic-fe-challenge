1f566b92727c3f18785252b147222d47e408311f
Miguel Ballen <mballen@endava.com>
2018-03-15 20:59:04 -0500

    Refactor initial html sketch

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,14 @@ function loadDoc() {
   xhttp.open("GET", "http://localhost:3000/", true);
   xhttp.send();
 }
+
+
+function toggleMenu() {
+    var menuBox = document.getElementById('menu-box');
+    if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+        menuBox.style.display = "none";
+    }
+    else { // if is menuBox hidden, display it
+        menuBox.style.display = "block";
+    }
+}
